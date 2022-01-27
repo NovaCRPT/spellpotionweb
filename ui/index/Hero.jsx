39cffb9a79d11514287675dafd4e9246ee03004f
JhosdryG <53978018+JhosdryG@users.noreply.github.com>
2022-01-27 11:17:09 -0400

    social media icon

--- ui/index/Hero.jsx
+++ ui/index/Hero.jsx
@@ -1,14 +1,20 @@
 import React, { useEffect, useRef } from "react";
 import Image from "next/image";
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import styles from "@styles/hero.module.sass";
 
 import heroBackground from "@assets/hero_background.png";
 import heroPotion from "@assets/hero_mobile_potion.png";
 import heroTitle from "@assets/hero_title.png";
 
+import discord from "@assets/discord-icon.png";
+import twitter from "@assets/twitter-icon.png";
+import youtube from "@assets/youtube-icon.png";
+
 const Hero = () => {
   const el = useRef();
+  gsap.registerPlugin(ScrollTrigger);
   const q = gsap.utils.selector(el);
   const tl = useRef();
 
@@ -18,7 +24,23 @@ const Hero = () => {
       .from(q(".hero_title"), { y: 50, opacity: 0, duration: 0.5 })
       .from(q(".hero_description"), { y: 50, opacity: 0, duration: 0.5 })
       .from(q(".hero_potion"), { y: 50, opacity: 0, duration: 0.7 })
+      .from(q(".socials a"), {
+        opacity: 0,
+        y: 20,
+        stagger: 0.2,
+        duration: 0.5,
+      })
       .from(q(".hero_timer"), { opacity: 0, duration: 0.5 });
+
+    gsap.from(q(".socials_fixed"), {
+      scrollTrigger: {
+        trigger: el.current,
+        start: "bottom center+=100px",
+        toggleActions: "play none none reverse",
+      },
+      opacity: 0,
+      duration: 0.5,
+    });
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
@@ -43,6 +65,7 @@ const Hero = () => {
                 layout="responsive"
                 src={heroTitle}
                 alt="title"
+                priority
               />
             </div>
             <p className="hero_description">
@@ -51,16 +74,49 @@ const Hero = () => {
               rarities, the value of each NFT depends on the value you give it
               based on your taste
             </p>
+            <div className={[styles.socials, "socials"].join(" ")}>
+              <a href="https://discord.gg/Phzv6ZAr" target="_blanck">
+                <Image src={discord} height={50} width={50} alt="discord" />
+              </a>
+              <a href="https://twitter.com/SpellPotions" target="_blanck">
+                <Image src={twitter} height={50} width={50} alt="discord" />
+              </a>
+              <a
+                href="https://www.youtube.com/channel/UCZ3hupUfwuAfbC9rWWugJ1w"
+                target="_blanck"
+              >
+                <Image src={youtube} height={50} width={50} alt="discord" />
+              </a>
+            </div>
           </div>
           <div className={styles.image_container}>
             <div className="hero_potion">
-              <Image layout="responsive" src={heroPotion} alt="drawn-potion" />
+              <Image
+                layout="responsive"
+                src={heroPotion}
+                priority
+                alt="drawn-potion"
+              />
             </div>
           </div>
         </div>
         <p className={[styles.time_counter, "hero_timer"].join(" ")}>
           20 days, 13 hours, 5 min, 23seg
         </p>
+      </div>
+      <div className={[styles.socials_fixed, "socials_fixed"].join(" ")}>
+        <a href="https://discord.gg/Phzv6ZAr" target="_blanck">
+          <Image src={discord} height={30} width={30} alt="discord" />
+        </a>
+        <a href="https://twitter.com/SpellPotions" target="_blanck">
+          <Image src={twitter} height={30} width={30} alt="discord" />
+        </a>
+        <a
+          href="https://www.youtube.com/channel/UCZ3hupUfwuAfbC9rWWugJ1w"
+          target="_blanck"
+        >
+          <Image src={youtube} height={30} width={30} alt="discord" />
+        </a>
       </div>
     </div>
   );
